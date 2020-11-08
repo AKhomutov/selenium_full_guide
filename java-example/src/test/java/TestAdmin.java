da81370c5570019f3096b2f7446a73653de4dfb6
Антон Хомутов <vorin20@gmail.com>
2020-11-08 16:12:16 +0200

    Task #7

--- java-example/src/test/java/TestAdmin.java
+++ java-example/src/test/java/TestAdmin.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.Assert.assertTrue;
+
 public class TestAdmin {
    private WebDriver wd;
    private WebDriverWait wait;
@@ -16,13 +18,35 @@ public class TestAdmin {
       wait = new WebDriverWait(wd, 10);
    }
    
-   @Test
-   public void testLoginAsAdmin() {
+   private void click(By by) {
+      wd.findElement(by).click();
+   }
+   
+   private void loginAsAdmin() {
       String adminCred = "admin";
       wd.get("http://localhost/litecart/admin/");
       wd.findElement(By.name("username")).sendKeys(adminCred);
       wd.findElement(By.name("password")).sendKeys(adminCred);
-      wd.findElement(By.name("login")).click();
+      click(By.name("login"));
+   }
+   
+   @Test
+   public void testLoginAsAdmin() {
+      loginAsAdmin();
+   }
+   
+   @Test
+   public void testOpenEverySection() {
+      loginAsAdmin();
+      int menu = wd.findElements(By.id("app-")).size();
+      for ( int i = 0; i < menu; i++){
+         wd.findElements(By.id("app-")).get(i).click();
+         int submenu = wd.findElements(By.cssSelector("#app- ul a")).size();
+         for (int l = 0; l < submenu; l++){
+            wd.findElements(By.cssSelector("#app- ul a")).get(l).click();
+            assertTrue(wd.findElement(By.cssSelector("h1")).isDisplayed());
+         }
+      }
    }
    
    @After
